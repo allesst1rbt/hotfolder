92383f84db709236a2c10ca3a03948854e037ff9
allesst1rbt <62967469+allesst1rbt@users.noreply.github.com>
2021-01-28 16:50:49 -0400

    Update watch.py

--- Projeto hotfolder/watch.py
+++ Projeto hotfolder/watch.py
@@ -5,35 +5,47 @@ import win32api
 import win32print
 import logging
 import shutil
+import ntpath
 from glob import glob
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from pathlib import Path
+
 
 def on_created(event):
+    time.sleep(1)
     print('created '+ event.src_path)
     path =str(event.src_path)
-    arraypath = path.split('.')
+    arraypath = path.split('\\')
     if path.find(".pdf") == -1:
         print("Ne pdf naum")
     else:
         impressoraVeia =win32print.GetDefaultPrinter()
-        win32print.SetDefaultPrinter('NOME DA IMPRESSORA AQUI')
-        pdf_dir = path
-        shutil.move(pdf_dir, "C:/Users/Public/notas"+arraypath[1].strip('/\/')+'.'+arraypath[2].strip('/\/'))
-        for f in glob("C:/Users/Public/notas"+arraypath[1].strip('/\/')+'.'+arraypath[2].strip('/\/'), recursive=True):
-           win32api.ShellExecute(0, "print", f, None,  ".",  0)   
+        win32print.SetDefaultPrinter('PDF Architect 8') 
+        
+        dst = os.path.join(r"C:/Users/Public/notas/",arraypath[-1])
+        dst = dst.replace('/', '\\')
+        src =(os.path.normpath(path_pai + arraypath[-1]))
+        src = src.replace('/', '\\')
+        copy2(src,dst)
+        win32api.ShellExecute(0, "print", dst, None,  ".",  0) 
+      
+         
         win32print.SetDefaultPrinter(impressoraVeia) 
-    exit          
+def copy2(src, dst, iteration=1000):
+   for x in range(iteration):
+	    with open(src, 'rb') as fsrc:
+	        with open(dst, 'wb') as fdst:
+		        fdst.write(fsrc.read())           
 
 if __name__ == '__main__':
     event_handler = FileSystemEventHandler()
 
     event_handler.on_created = on_created
-  
-
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    global path_pai
+    path_pai =str(Path.home())+'\\Downloads\\'
     observer = Observer()
-    observer.schedule(event_handler,path,recursive=True)
+    observer.schedule(event_handler,path_pai,recursive=True)
     observer.start()
     
     try:
